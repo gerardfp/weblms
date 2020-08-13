ed6fdf8441976e538879a80a6a50af32b2a1e596
root <root@myblog.lxd>
2020-08-13 10:10:24 +0000

    ok

---
+++ index.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="style.css">
+
+<?php
+$mysqli = new mysqli("localhost", "my_lmsuser", "my_lmspassword", "my_lmsdb");
+
+$resultat = $mysqli->query("SELECT * FROM questions");
+
+echo "<form action='review.php' method='GET'>";
+while ($fila = $resultat->fetch_assoc()) {
+	$id = $fila['id'];
+	$pregunta = $fila['q'];
+
+	echo "<div class='question'>";
+	echo "<p>$pregunta</p>\n";
+	for($i=1; $i<=4; $i++){
+		echo "<div class='answer'><input type='radio' id='$id$i' name='$id' value='$i'><label for='$id$i'>" . $fila["a$i"] . "</label></div>";
+	}
+	echo "</div>";
+}
+echo "<input type='submit' value=\"Acaba l'intent...\">";
+echo "</form>";
+?>
---
+++ review.php
@@ -0,0 +1,48 @@
+<link rel="stylesheet" href="style.css">
+
+<?php
+
+$mysqli = new mysqli("localhost", "my_lmsuser", "my_lmspassword", "my_lmsdb");
+
+$resultat = $mysqli->query("SELECT * FROM questions");
+
+$punts = 0;
+$total = 0;
+
+while ($fila = $resultat->fetch_assoc()) {
+	$total++;
+
+	$id = $fila['id'];
+	$pregunta = $fila['q'];
+	echo "<div class='question'>";
+	echo "<p>$pregunta</p>";
+
+	for($i=1; $i<=4; $i++){
+		if($_GET[$id] == $i){
+			$marcada = "checked";
+                	if($_GET[$id] == $fila['correct']){
+				$classe = "correct";
+				$punts++;
+                	} else {
+				$classe = "incorrect";
+                	}
+        	} else {
+			$marcada = "";
+			if($fila['correct'] == $i){
+				$classe = "valid";
+			}else {
+				$classe = "";
+			}
+        	}
+		echo "<div class='answer $classe'><input type='radio' id='answer$i' $marcada disabled><label for='answer$i'>" . $fila["a$i"] . "</label></div>";
+	}
+
+	if($_GET[$id] == $fila['correct']){
+		echo "<br>+1!";
+	}
+	echo "</div>";
+
+}
+echo "<p>Puntuació: $punts de $total</p>";
+
+?>
